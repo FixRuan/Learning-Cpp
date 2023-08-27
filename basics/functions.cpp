3d10a72febf1098c6dab8da4c1d4471ccda0173a
fixruan <ruangoio01@gmail.com>
2023-08-27 18:04:34 -0300

    :sparkles: feat: functions in C++

--- basics/functions.cpp
+++ basics/functions.cpp
@@ -2,7 +2,16 @@
 
 using namespace std;
 
-int main() {
+void sayHello() {
+  cout << "Hello World" << endl;
+}
 
+int sum(int a, int b) {
+  return a + b;
+}
+
+int main() {
+  sayHello();
+  cout << sum(10, 10);
   return 0;
 }
